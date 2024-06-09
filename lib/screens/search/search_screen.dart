8e86f119a265cd6b2633a90d3474ac552f40d0c6
Madhur21G <127782455+Madhur21G@users.noreply.github.com>
2024-06-09 16:57:36 +0530

    search page done

--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:ticket_project/Base/res/media.dart';
 import 'package:ticket_project/Base/res/styles/app_styles.dart';
 import 'package:ticket_project/Base/utils/app_routes.dart';
 import 'package:ticket_project/Base/widgets/app_double_text.dart';
 import 'package:ticket_project/screens/search/widgets/app_text_icon.dart';
 import 'package:ticket_project/screens/search/widgets/app_ticket_tabs.dart';
 import 'package:ticket_project/screens/search/widgets/find_tickets.dart';
+import 'package:ticket_project/screens/search/widgets/ticket_promotions.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: AppStyles.bgcolor,
       body: ListView(
@@ -35,47 +33,7 @@ class SearchScreen extends StatelessWidget {
             func:  () => Navigator.pushNamed(context, AppRoutes.allTickets),
           ),
           const SizedBox(height: 15,),
-          Row(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-                width: size.width*.42,
-                height: 405,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.white,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.shade200,
-                      blurRadius: 1,
-                      spreadRadius: 2,
-                    )
-                  ]
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: 190,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        image: const DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            AppMedia.planesit
-                          )
-                        )
-                      ),
-                    ),
-                    const SizedBox(height: 12,),
-                    Text(
-                      "20% discount on the early booking of this flight. Don't miss",
-                      style: AppStyles.headLineStyle2,
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
+          const TicketPromotions(),
         ],
       ),
     );
